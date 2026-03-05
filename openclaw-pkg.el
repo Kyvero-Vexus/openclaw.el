a834e9af4dc867d0a90beac1ac05431bc21d7539
Chrysolambda-ops <chrysolambda-ops@gmail.com>
2026-03-05 02:35:30 -0500

    Add straight.el installation support
    - Add openclaw-pkg.el for package metadata
    - Update README with straight.el and Quelpa installation options
    - Document use-package integration with straight

---
+++ openclaw-pkg.el
@@ -0,0 +1,8 @@
+;; Package definition for package.el and straight.el
+(define-package "openclaw" "0.1.0"
+  "Emacs interface to OpenClaw"
+  '((emacs "27.1")
+    (websocket "1.15"))
+  :url "https://github.com/Kyvero-Vexus/openclaw.el"
+  :keywords '("comm" "tools" "ai")
+  :license "AGPL-3.0-or-later")
